350f48955c2f37afb150f2e2e26e5da6f8ea988d
Negel Artem <artemnegel@gmail.com>
2018-12-09 14:02:26 +0200

    initial commit

---
+++ app/src/main/java/com/example/acron/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.acron.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //hi, it's first commit!
+    }
+}
